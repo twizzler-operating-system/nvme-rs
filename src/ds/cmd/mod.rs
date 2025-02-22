4a6ee8661f2de3a66096618b729df02f496ef4a2
Daniel Bittman <danielbittman1@gmail.com>
2025-02-21 21:06:53 -0800

    Get dptr.

--- src/ds/cmd/mod.rs
+++ src/ds/cmd/mod.rs
@@ -1,4 +1,4 @@
-use super::Address;
+use super::{queue::subentry::Dptr, Address};
 
 pub mod admin;
 
@@ -6,4 +6,14 @@ pub enum PrpListOrBuffer {
     PrpList(Address),
     PrpFirstAndList(Address, Address),
     Buffer(Address),
+}
+
+impl PrpListOrBuffer {
+    pub fn dptr(&self) -> Dptr {
+        match self {
+            PrpListOrBuffer::PrpList(address) => Dptr::Prp(*address, 0),
+            PrpListOrBuffer::Buffer(address) => Dptr::Prp(*address, 0),
+            PrpListOrBuffer::PrpFirstAndList(first, list) => Dptr::Prp(*first, *list),
+        }
+    }
 }
